c7f5511badd2012b30628379f8ef82d205ce372b
Raptoorka <lusy.buresova@seznam.cz>
2021-01-26 21:46:14 +0100

    Add route for accessory.

--- src/Controller/MaterialController.php
+++ src/Controller/MaterialController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Accessory;
 use App\Entity\Material;
+use App\Form\AccessoryType;
 use App\Form\MaterialType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,29 +19,45 @@ class MaterialController extends AbstractController
     public function index(Request $request): Response
     {
         $material = new Material();
-        $form = $this->createForm(MaterialType::class, $material);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $materialData = $form->getData();
+        $accessory = new Accessory();
+
+        $formMaterial = $this->createForm(MaterialType::class, $material);
+        $formMaterial->handleRequest($request);
+        if ($formMaterial->isSubmitted() && $formMaterial->isValid()) {
+            $materialData = $formMaterial->getData();
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($materialData );
             $entityManager->flush();
             return $this->redirectToRoute("material");
         }
+
+        $formAccessory = $this->createForm(AccessoryType::class, $accessory);
+        $formAccessory->handleRequest($request);
+
+        if ($formAccessory->isSubmitted() && $formAccessory->isValid()) {
+            $accessoryData = $formAccessory->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($accessoryData );
+            $entityManager->flush();
+            return $this->redirectToRoute("material");
+        }
+
         $materials = $this->getDoctrine()->getRepository(Material::class)->findAll();
+        $accessories = $this->getDoctrine()->getRepository(Accessory::class)->findAll();
         return $this->render('material/index.html.twig', [
+            'accessories' => $accessories,
             'materials' => $materials,
-            'form' => $form->createView()
+            'formMaterial' => $formMaterial->createView(),
+            'formAccessory' => $formAccessory->createView()
         ]);
     }
 
     /**
-     * @Route("/material/{id}", name="materialUpdate")
+     * @Route("/materialUpdate/{id}", name="materialUpdate")
      */
     public function materialUpdate(int $id, Request $request): Response
     {
         $entityRepository = $this->getDoctrine()->getRepository(Material::class);
-        $materials = $entityRepository->findAll();
 
         $material = $entityRepository->find($id);
 
@@ -57,10 +75,39 @@ class MaterialController extends AbstractController
             return $this->redirectToRoute("material");
         }
 
-        return $this->render('material/index.html.twig', [
-            'materials' => $materials,
-            'form' => $form->createView(),
-            'id' => $id
+        return $this->render('material/updateMaterial.html.twig', [
+            'formMaterial' => $form->createView(),
+            'materialId' => $id
+        ]);
+    }
+
+    /**
+     * @Route("/accessoryUpdate/{id}", name="accesoryUpdate")
+     */
+    public function accessoryUpdate(int $id, Request $request): Response
+    {
+        $entityRepository = $this->getDoctrine()->getRepository(Accessory::class);
+
+
+        $accessory = $entityRepository->find($id);
+
+        $form = $this->createForm(AccessoryType::class, $accessory);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $event = $form->getData();
+            if ($form->get('delete')->isClicked()) {
+                $entityManager->remove($event);
+            } else {
+                $entityManager->persist($event);
+            }
+            $entityManager->flush();
+            return $this->redirectToRoute("material");
+        }
+
+        return $this->render('material/updateAccessory.html.twig', [
+            'formAccessory' => $form->createView(),
+            'accessoryId' => $id
         ]);
     }
 }
